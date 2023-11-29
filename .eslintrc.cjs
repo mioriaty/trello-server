9682283cd0fd14e8ef23918f3e005928498c2c93
duongto <mioriaty@gmail.com>
2023-11-29 21:38:15 +0700

    update folder structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,7 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- * Sample Eslint config for NodeJS ExpressJS MongoDB project
- */
 module.exports = {
   env: { es2020: true, node: true },
   extends: [
-    'eslint:recommended', 'prettier'
+    'eslint:recommended', 'prettier', 'eslint-config-prettier'
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
 
@@ -9,15 +8,19 @@ const port = 8017
 
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-    { id: 'id-2', name: 'Two' },
-    { id: 'id-3', name: 'Three' },
-    { id: 'id-4', name: 'Four' },
-    { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
   res.end('<h1>Hello!</h1><hr>')
 })
 
